8d145ed4024e8f10147805893aea37639319b8ce
Vincent Adams <61333244+Vince-ctrl@users.noreply.github.com>
2025-12-18 22:07:47 -0700

    Update Cards.java

--- Cards.java
+++ Cards.java
@@ -1,123 +1,89 @@
 import java.util.ArrayList;
 import java.util.Collections;
+
 public class Cards {
     String suit;
     String value;
-    static ArrayList<Cards> Deck = new ArrayList<Cards>();
-    static ArrayList<Cards> hand1 = new ArrayList<Cards>();
-    static ArrayList<Cards> hand2 = new ArrayList<Cards>();
-    static ArrayList<Cards> hand3 = new ArrayList<Cards>();
-    static ArrayList<Cards> hand4 = new ArrayList<Cards>();
-    //constructor and a lot of values
-    public Cards(int suit, int value){
-        if (suit < 4) {
-            if (suit == 0) {
-                this.suit = "Hearts";
-            }
-            if (suit == 1) {
-                this.suit = "Diamonds";
-            }
-            if (suit == 2) {
-                this.suit = "Clubs";
-            }
-            if (suit == 3) {
-                this.suit = "Spades";
-            }
-        } else {
-            System.err.println("Out of bounds suits error");
+    static ArrayList<Cards> Deck = new ArrayList<>();
+    static ArrayList<Cards> hand1 = new ArrayList<>();
+    static ArrayList<Cards> hand2 = new ArrayList<>();
+    static ArrayList<Cards> hand3 = new ArrayList<>();
+    static ArrayList<Cards> hand4 = new ArrayList<>();
+
+    // Constructor
+    public Cards(int suit, int value) {
+        switch (suit) {
+            case 0: this.suit = "Hearts"; break;
+            case 1: this.suit = "Diamonds"; break;
+            case 2: this.suit = "Clubs"; break;
+            case 3: this.suit = "Spades"; break;
+            default: throw new IllegalArgumentException("Invalid suit index");
         }
-        if (value < 13) {
-            if (value == 0) {
-                this.value = "Ace";
-            }
-            if (value == 1) {
-                this.value = "2";
-            }
-            if (value == 2) {
-                this.value = "3";
-            }
-            if (value == 3) {
-                this.value = "4";
-            }
-            if (value == 4) {
-                this.value = "5";
-            }
-            if (value == 5) {
-                this.value = "6";
-            }
-            if (value == 6) {
-                this.value = "7";
-            }
-            if (value == 7) {
-                this.value = "8";
-            }
-            if (value == 8) {
-                this.value = "9";
-            }
-            if (value == 9) {
-                this.value = "10";
-            }
-            if (value == 10) {
-                this.value = "Jack";
-            }
-            if (value == 11) {
-                this.value = "Queen";
-            }
-            if (value == 12) {
-                this.value = "King";
-            }
-        } else {
-            System.err.println("Out of bounds value error");
+
+        switch (value) {
+            case 0: this.value = "Ace"; break;
+            case 1: this.value = "2"; break;
+            case 2: this.value = "3"; break;
+            case 3: this.value = "4"; break;
+            case 4: this.value = "5"; break;
+            case 5: this.value = "6"; break;
+            case 6: this.value = "7"; break;
+            case 7: this.value = "8"; break;
+            case 8: this.value = "9"; break;
+            case 9: this.value = "10"; break;
+            case 10: this.value = "Jack"; break;
+            case 11: this.value = "Queen"; break;
+            case 12: this.value = "King"; break;
+            default: throw new IllegalArgumentException("Invalid value index");
         }
     }
-    public String toString(){
+
+    public String toString() {
         return value + " of " + suit;
     }
-    public static void createDeck(int tempSuit, int tempValue){
-        tempSuit = 0;
-        tempValue = 0;
-        while (tempSuit < 4) {
-            if (tempValue < 13) {
-                Deck.add(new Cards(tempSuit, tempValue));
-                tempValue++;
-            } else {
-                tempSuit++;
-                tempValue = 0;
+
+    // Build full deck
+    public static void createDeck() {
+        Deck.clear();
+        for (int s = 0; s < 4; s++) {
+            for (int v = 0; v < 13; v++) {
+                Deck.add(new Cards(s, v));
             }
         }
     }
-    public static void shuffleDeck(){
+
+    // Shuffle deck
+    public static void shuffleDeck() {
         Collections.shuffle(Deck);
     }
-    public static void dealHands(){
-        int cardIndex = 0;
-        int handIndex = 0;
-        while (cardIndex > 52) {
-            while (handIndex == 0) {
-                hand1.add(Deck.get(cardIndex));
-                cardIndex++;
-                if (cardIndex == 12) {
-                    handIndex++;
-                }
-            }
-            while (handIndex == 1) {
-                hand2.add(Deck.get(cardIndex));
-                cardIndex++;
-                if (cardIndex == 25) {
-                    handIndex++;
-                }
-            }
-            while (handIndex == 2) {
-                hand3.add(Deck.get(cardIndex));
-                cardIndex++;
-                if (cardIndex == 38) {
-                    handIndex++;
-                }
-            }
-            while (handIndex == 3) {
-                hand4.add(Deck.get(cardIndex));
-                cardIndex++;
+
+    // Deal evenly into 4 hands
+    public static void dealHands() {
+        hand1.clear();
+        hand2.clear();
+        hand3.clear();
+        hand4.clear();
+
+        for (int i = 0; i < Deck.size(); i++) {
+            Cards card = Deck.get(i);
+            switch (i % 4) {
+                case 0: hand1.add(card); break;
+                case 1: hand2.add(card); break;
+                case 2: hand3.add(card); break;
+                case 3: hand4.add(card); break;
             }
         }
+    }
+
+    // Quick test
+    public static void main(String[] args) {
+        createDeck();
+        shuffleDeck();
+        dealHands();
+
+        System.out.println("Hand 1: " + hand1);
+        System.out.println("Hand 2: " + hand2);
+        System.out.println("Hand 3: " + hand3);
+        System.out.println("Hand 4: " + hand4);
     }
 }
